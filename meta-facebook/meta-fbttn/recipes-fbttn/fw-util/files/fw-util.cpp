102f527d346514749fb13bcdc5fbd7945daf22a7
Alejandro Enedino Hernandez Samaniego <aehs29@gmail.com>
2019-07-06 20:45:04 -0700

    fw-util: Port the fw-util and fw-util-test recipes to meta-fbttn
    Given that the BBFILE_PRIORITY of the common layer is set to 6
    and the one for meta-fbttn is set to 20, the recipe fw-util is
    being forced to be taken from the meta-fbttn layer (which only
    contains v0.1 and fails to build at this point).
    
    This used to work on previous poky branches but the implementation
    of BBFILE_PRIORITY changed / was fixed on newer branches, which
    allows us to see this error that we were not able to see before
    even if it already existed.
    
    The following is taken from the definition of BBFILE_PRIORITY in
    the Yocto reference manual:
    
    "The precedence established through this variable stands regardless of a
    recipe's version (PV variable). For example, a layer that has a recipe
    with a higher PV value but for which the BBFILE_PRIORITY is set to have
    a lower precedence still has a lower precedence."
    
    Since the logical way is still to keep meta-fbttn with a higher
    BBFILE_PRIORITY, we have to port the recipe from the common layer
    to meta-fbttn to be able to build it correctly (v0.2)
    
    Signed-off-by: Alejandro Enedino Hernandez Samaniego <aehs29@gmail.com>

---
+++ meta-facebook/meta-fbttn/recipes-fbttn/fw-util/files/fw-util.cpp
@@ -0,0 +1,332 @@
+/*
+ * fw-util.cpp
+ *
+ * Copyright 2017-present Facebook. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <map>
+#include <tuple>
+#ifdef __TEST__
+#include <gtest/gtest.h>
+#endif
+#include "fw-util.h"
+
+using namespace std;
+
+string exec_name = "Unknown";
+map<string, map<string, Component *>> * Component::fru_list = NULL;
+
+Component::Component(string fru, string component)
+  : _fru(fru), _component(component)
+{
+  fru_list_setup();
+  (*fru_list)[fru][component] = this;
+}
+
+Component::~Component()
+{
+  (*fru_list)[_fru].erase(_component);
+  if ((*fru_list)[_fru].size() == 0) {
+    fru_list->erase(_fru);
+  }
+}
+
+Component *Component::find_component(string fru, string comp)
+{
+  if (!fru_list) {
+    return NULL;
+  }
+  if (fru_list->find(fru) == fru_list->end()) {
+    return NULL;
+  }
+  if ((*fru_list)[fru].find(comp) == (*fru_list)[fru].end()) {
+    return NULL;
+  }
+  return (*fru_list)[fru][comp];
+}
+
+AliasComponent::AliasComponent(string fru, string comp, string t_fru, string t_comp) :
+  Component(fru, comp), _target_fru(t_fru), _target_comp_name(t_comp), _target_comp(NULL)
+{
+  // This might not be successful this early.
+  _target_comp = find_component(t_fru, t_comp);
+}
+
+bool AliasComponent::setup()
+{
+  if (!_target_comp) {
+    _target_comp = find_component(_target_fru, _target_comp_name);
+    if (!_target_comp) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int AliasComponent::update(string image)
+{
+  if (!setup())
+    return FW_STATUS_NOT_SUPPORTED;
+  return _target_comp->update(image);
+}
+
+int AliasComponent::fupdate(string image)
+{
+  if (!setup())
+    return FW_STATUS_NOT_SUPPORTED;
+  return _target_comp->fupdate(image);
+}
+
+int AliasComponent::dump(string image)
+{
+  if (!setup())
+    return FW_STATUS_NOT_SUPPORTED;
+  return _target_comp->dump(image);
+}
+
+int AliasComponent::print_version()
+{
+  if (!setup())
+    return FW_STATUS_NOT_SUPPORTED;
+  return _target_comp->print_version();
+}
+
+class ProcessLock {
+  private:
+    string file;
+    int fd;
+    bool _ok;
+  public:
+  ProcessLock(string file) {
+    _ok = false;
+    fd = open(file.c_str(), O_RDWR|O_CREAT, 0666);
+    if (fd < 0) {
+      return;
+    }
+    if (flock(fd, LOCK_EX | LOCK_NB) < 0) {
+      close(fd);
+      fd = -1;
+      return;
+    }
+    _ok = true;
+  }
+  ~ProcessLock() {
+    if (_ok) {
+      remove(file.c_str());
+      close(fd);
+    }
+  }
+  bool ok() {
+    return _ok;
+  }
+};
+
+void usage()
+{
+  cout << "USAGE: " << exec_name << " all|FRU --version [all|COMPONENT]" << endl;
+  cout << "       " << exec_name << " FRU --update [--]COMPONENT IMAGE_PATH" << endl;
+  cout << "       " << exec_name << " FRU --force --update [--]COMPONENT IMAGE_PATH" << endl;
+  cout << "       " << exec_name << " FRU --dump [--]COMPONENT IMAGE_PATH" << endl;
+  cout << left << setw(10) << "FRU" << " : Components" << endl;
+  cout << "---------- : ----------" << endl;
+  for (auto fkv : *Component::fru_list) {
+    string fru_name = fkv.first;
+    cout << left << setw(10) << fru_name << " : ";
+    for (auto ckv : fkv.second) {
+      string comp_name = ckv.first;
+      Component *c = ckv.second;
+      cout << comp_name;
+      if (c->is_alias()) {
+        AliasComponent *a = (AliasComponent *)c;
+        cout << "(" << a->alias_fru() << ":" << a->alias_component() << ")";
+      }
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int ret = 0;
+  int find_comp = 0;
+
+  Component::fru_list_setup();
+
+#ifdef __TEST__
+  testing::InitGoogleTest(&argc, argv);
+  ret = RUN_ALL_TESTS();
+  if (ret != 0) {
+    return ret;
+  }
+#endif
+
+  System system;
+
+  exec_name = argv[0];
+  if (argc < 3) {
+    usage();
+    return -1;
+  }
+
+  string fru(argv[1]);
+  string action(argv[2]);
+  string component("all");
+  string image("");
+
+  if (action == "--force") {
+    if (argc < 4) {
+      usage();
+      return -1;
+    }
+    string action_ext(argv[3]);
+    if (action_ext != "--update") {
+      usage();
+      return -1;
+    }
+    if (argc >= 5) {
+      component.assign(argv[4]);
+      if (component.compare(0, 2, "--") == 0) {
+        component = component.substr(2);
+      }
+    }
+  } else {
+    if (argc >= 4) {
+      component.assign(argv[3]);
+      if (component.compare(0, 2, "--") == 0) {
+        component = component.substr(2);
+      }
+    }
+  }
+
+  if ((action == "--update") || (action == "--dump")) {
+    if (argc != 5) {
+      usage();
+      return -1;
+    }
+    image.assign(argv[4]);
+    if (action == "--update") {
+      ifstream f(image);
+      if (!f.good()) {
+        cerr << "Cannot access: " << image << endl;
+        return -1;
+      }
+    } 
+    if (component == "all") {
+      cerr << "Upgrading all components not supported" << endl;
+      return -1;
+    }
+  } else if (action == "--force") {
+    if (argc != 6) {
+      usage();
+      return -1;
+    }
+    image.assign(argv[5]);
+    ifstream f(image);
+    if (!f.good()) {
+      cerr << "Cannot access: " << image << endl;
+      return -1;
+    }
+    if (component == "all") {
+      cerr << "Upgrading all components not supported" << endl;
+      return -1;
+    }
+  } else if (action == "--version") {
+    if(argc > 4) {
+      usage();
+      return -1;
+    }
+  } else {
+    cerr << "Invalid action: " << action << endl;
+    usage();
+    return -1;
+  }
+
+  for (auto fkv : *Component::fru_list) {
+    if (fru == "all" || fru == fkv.first) {
+      // Ensure only one instance of fw-util per FRU is running
+      string this_fru(fkv.first);
+      ProcessLock lock(system.lock_file(this_fru));
+      if (!lock.ok()) {
+        cerr << "Another instance of fw-util already running" << endl;
+        return -1;
+      }
+
+      for (auto ckv : fkv.second) {
+        if (component == "all" || component == ckv.first) {
+          find_comp = 1;
+          Component *c = ckv.second;
+
+          // Ignore aliases if their target is going to be
+          // considered in one of the loops.
+          if (c->is_alias()) {
+            if (component == "all" && (fru == "all" || fru == c->alias_fru()))
+              continue;
+            if (fru == "all" && component == c->alias_component())
+              continue;
+          }
+
+          if (action == "--version") {
+            ret = c->print_version();
+            if (ret != FW_STATUS_SUCCESS && ret != FW_STATUS_NOT_SUPPORTED) {
+              cerr << "Error getting version of " << c->component()
+                << " on fru: " << c->fru() << endl;
+            }
+          } else {  // update or dump
+            string str_act("");
+            uint8_t fru_id = system.get_fru_id(c->fru());
+            system.set_update_ongoing(fru_id, 60 * 10);
+            if (action == "--update") {
+              ret = c->update(image);
+              str_act.assign("Upgrade");
+            } else if (action == "--force") {
+              ret = c->fupdate(image);
+              str_act.assign("Force upgrade");
+            } else {
+              ret = c->dump(image);
+              str_act.assign("Dump");
+            }
+            system.set_update_ongoing(fru_id, 0);
+            if (ret == 0) {
+              cout << str_act << " of " << c->fru() << " : " << component << " succeeded" << endl;
+            } else {
+              cerr << str_act << " of " << c->fru() << " : " << component;
+              if (ret == FW_STATUS_NOT_SUPPORTED) {
+                cerr << " not supported" << endl;
+              } else {
+                cerr << " failed" << endl;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  if (!find_comp) {
+    usage();
+    return -1;
+  }
+
+  return 0;
+}
